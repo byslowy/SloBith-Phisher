09d5825cf5ad73b903929cdcc78906d2b5fb431c
byslowy <151045503+byslowy@users.noreply.github.com>
2023-12-14 21:38:22 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,15 +1,7 @@
 import os
 import sys
-
-def self_destruct():
-    try:
-        os.remove(sys.argv[0])
-      
-    except Exception as e:
-        print(f"Hata oluþtu: {e}")
-
 if __name__ == "__main__":
-    print("\033[34mBu dosyayý bir kere kullanmanýz yeterlidir!!!")
+    print("\033[34mBu dosyayÃ½ bir kere kullanmanÃ½z yeterlidir!!!")
     time.sleep(1)
     os.system("clear")
     os.system("chmod +x slobit.py")
@@ -17,7 +9,7 @@ if __name__ == "__main__":
     os.system("clear")
     print("""\033[32m
     ###############################################################################
-    # HER ÞEY TAMAM! SADECE "python slophis.py" diyerek tool'u çalýþtýrabilirsiniz! #
+    # HER ÃžEY TAMAM! SADECE "python slophis.py" diyerek tool'u Ã§alÃ½Ã¾tÃ½rabilirsiniz! #
     ###############################################################################
     """)
-    self_destruct()
+    
