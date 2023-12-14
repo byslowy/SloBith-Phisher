b34e4581579d49d94381ebd031b84e2537a32677
byslowy <151045503+byslowy@users.noreply.github.com>
2023-12-14 21:43:44 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,10 +1,10 @@
 import os
-import sys
+import time
 if __name__ == "__main__":
     print("\033[34mBu dosyayý bir kere kullanmanýz yeterlidir!!!")
     time.sleep(1)
     os.system("clear")
-    os.system("chmod +x slobit.py")
+    os.system("chmod +x phish.py")
     os.system("pip install requests")
     os.system("clear")
     print("""\033[32m
